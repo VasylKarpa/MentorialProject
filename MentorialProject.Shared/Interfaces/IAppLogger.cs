33c824b22dc25fc302c25e92d82e2369a5e8be98
Vasyl' Karpa <vkarpa@softserveinc.com>
2019-04-17 17:03:19 +0300

    Added Logger

---
+++ MentorialProject.Shared/Interfaces/IAppLogger.cs
@@ -0,0 +1,6 @@
+﻿namespace MentorialProject.Shared.Interfaces {
+  public interface IAppLogger<T> {
+    void LogInformation(string message, params object[] args);
+    void LogWarning(string message, params object[] args);
+  }
+}
